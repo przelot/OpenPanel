6800d8b1cafc98dc5df25b1462de6ffb5e568fce
Fabian Worzała <fawocode@gmail.com>
2024-11-29 16:52:13 +0100

    More functions to client

--- openpanel-client.cc
+++ openpanel-client.cc
@@ -1,4 +1,38 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+// Define command to clear terminal
+#ifdef __linux__
+    char clear_screen_command[6] = "clear";
+#elif _WIN32 or _WIN64
+    char clear_screen_command[4] = "cls";
+#else
+    std::cout << "Undefined operating system" << std::endl;
+    char clear_screen_command[5] = "echo"
+#endif
+
+void clear_screen() {
+    // Clear the terminal
+    system(clear_screen_command);
+}
+
+void blankline() {
+    // Leave an empty line
+    std::cout << '\n';
+}
+
+void sleep(int seconds) {
+    // Do nothing for X seconds
+    std::this_thread::sleep_for(std::chrono::seconds(seconds));
+}
+
+void get_time() {
+    // Get current time
+    auto time = std::chrono::system_clock::now();
+    std::time_t time_now = std::chrono::system_clock::to_time_t(time);
+    std::cout << "Current time: " << std::ctime(&time_now);
+}
 
 int main(int argc, char const *argv[])
 {

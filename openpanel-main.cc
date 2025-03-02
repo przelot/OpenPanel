11a66f6545503f2dfcf806969c5ddc91050e8bc5
Fabian Worzała <fawocode@gmail.com>
2025-03-02 15:50:06 +0100

    Single client socket

--- openpanel-main.cc
+++ openpanel-main.cc
@@ -349,9 +349,41 @@ void socket_server() {
     }
     std::cout << "[OK] Starting " << modeFromConfig << std::endl;
     if(modeFromConfig == "SERVER") {
-        // Server mode
+        // creating socket
+        int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        // specifying the address
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(8080);
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+        // binding socket.
+        bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+        // listening to the assigned socket
+        listen(serverSocket, 5);
+        // accepting connection request
+        int clientSocket = accept(serverSocket, nullptr, nullptr);
+        // recieving data
+        char buffer[1024] = { 0 };
+        recv(clientSocket, buffer, sizeof(buffer), 0);
+        std::cout << "Message from client: " << buffer << std::endl;
+        // closing the socket.
+        close(serverSocket);
+        return;
     } else if(modeFromConfig == "CLIENT") {
-        // Client mode
+        int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        // specifying address
+        sockaddr_in serverAddress;
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(8080);
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+        // sending connection request
+        connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+        // sending data
+        const char* message = "[OK] Connection established";
+        send(clientSocket, message, strlen(message), 0);
+        // closing socket
+        close(clientSocket);
+        return;
     } else {
         std::cout << "[ERROR] Unknown socket mode" << std::endl;
     }
